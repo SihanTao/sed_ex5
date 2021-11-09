c888e4960228224c449b209aa28ffae125e91b4c
Sihan-Tao <sihan.tao20@imperial.ac.uk>
2021-11-09 18:07:36 +0000

    Use Latch in DownloadTask

--- src/main/java/ic/doc/DownloadTask.java
+++ src/main/java/ic/doc/DownloadTask.java
@@ -4,17 +4,21 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.CountDownLatch;
 
 public class DownloadTask implements Runnable {
 
   private final URL url;
   private final Path downloadDirectory;
   private final String filename;
+  private final CountDownLatch latch;
 
-  public DownloadTask(URL url, Path downloadDirectory, String filename) {
+  public DownloadTask(URL url, Path downloadDirectory, String filename,
+      CountDownLatch latch) {
     this.url = url;
     this.downloadDirectory = downloadDirectory;
     this.filename = filename;
+    this.latch = latch;
   }
 
   @Override
@@ -27,6 +31,8 @@ public class DownloadTask implements Runnable {
       throw new DownloadException(e);
     }
     System.out.println("Finished downloading " + filename);
+
+    latch.countDown();
   }
 
 }
